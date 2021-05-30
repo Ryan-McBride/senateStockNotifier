104596edad804bb66728494473d79a614a6ef9e4
Ryan-McBride <mcbrideryan89@gmail.com>
2021-05-30 02:11:09 -0700

    almost there

--- index.js
+++ index.js
@@ -2,58 +2,33 @@ const fs = require('fs');
 const axios = require('axios').default;
 const nodemailer = require('nodemailer');
 const parser = require('xml2json');
+const _ = require('lodash');
 
-const reports = [];
-axios.get('https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/filemap.xml')
-  .then((resp) => {
-    if (fs.existsSync('./.housesnapshot.xml')) {
-      const oldSnapshot = fs.readFileSync('./.housesnapshot.xml', 'utf-8');
-      const json = JSON.parse(parser.toJson(oldSnapshot));
-      const latest = json.listBucketFileMap.Contents[0].Key.replace(/"/g, '');
-      axios.get(`https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/${latest}`)
-        .then((report) => {
-          report.data.forEach((person) => {
-            person.transactions.forEach((transaction) => {
-              if (transaction.ticker !== 'NWN') {
-                reports.push(`${transaction.ticker}: ${transaction.transaction_type}`);
-              }
-            });
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    } else {
-      fs.writeFileSync('.housesnapshot.xml', resp.data);
-    }
+const getReports = async (url) => axios.get(url);
+const convertReports = (data) => {
+  const conv = JSON.parse(parser.toJson(data)).listBucketFileMap.Contents.map((val) => val.Key);
+  return conv;
+};
+const getSnapshot = (type) => JSON.parse(fs.readFileSync(`./.${type}Snapshot.json`));
+const getTransactions = (reports, url) => Promise.all(reports.map((report) => axios.get(`${url}/${report}`)))
+  .then((transactions) => {
+    const t = transactions.map((val) => val.data);
+    return t.flat().map((val) => val.transactions.flat());
   })
-  .catch((error) => {
-    console.log(error);
+  .catch((err) => {
+    console.log(err);
   });
 
-axios.get('https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/aggregate/filemap.xml')
-  .then((resp) => {
-    if (fs.existsSync('./.housesnapshot.xml')) {
-      const oldSnapshot = fs.readFileSync('./.housesnapshot.xml', 'utf-8');
-      const json = JSON.parse(parser.toJson(oldSnapshot));
-      const latest = json.listBucketFileMap.Contents[0].Key.replace(/"/g, '');
-      axios.get(`https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/${latest}`)
-        .then((report) => {
-          report.data.forEach((person) => {
-            person.transactions.forEach((transaction) => {
-              if (transaction.ticker !== 'NWN') {
-                reports.push(`${transaction.ticker}: ${transaction.transaction_type}`);
-              }
-            });
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    } else {
-      fs.writeFileSync('.housesnapshot.xml', resp.data);
+getReports('https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/aggregate/filemap.xml')
+  .then((reports) => convertReports(reports.data))
+  .then((converted) => {
+    const newReports = [];
+    const snapshot = getSnapshot('senate');
+    while (!_.isEqual(converted, snapshot)) {
+      newReports.push(converted.shift());
     }
+    return getTransactions(newReports, 'https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com');
   })
-  .catch((error) => {
-    console.log(error);
+  .then((reports) => {
+    console.log(reports);
   });
