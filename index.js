bf68f723fd594991035f762663e304db838fcd26
Ryan-McBride <mcbrideryan89@gmail.com>
2021-06-14 09:05:39 -0700

    senate and house working

--- index.js
+++ index.js
@@ -18,17 +18,44 @@ const getTransactions = (reports, url) => Promise.all(reports.map((report) => ax
   .catch((err) => {
     console.log(err);
   });
+const getNewReports = (converted, type) => {
+  const newReports = [];
+  const snapshot = getSnapshot(type);
+  while (!_.isEqual(converted, snapshot)) {
+    newReports.push(converted.shift());
+  }
+  return newReports;
+};
+const getValidTickers = ((reports) => reports.flat().filter((report) => report.ticker !== '--'));
+const prepareText = ((reports) => reports.map((val) => `${val.type || val.transaction_type} ${val.ticker}`));
+const storeSnap = ((content, type) => {
+  fs.writeFileSync(`./.${type}Snapshot.json`, JSON.stringify(content));
+});
+
+const email = [];
+let fileOutput;
 
 getReports('https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/aggregate/filemap.xml')
   .then((reports) => convertReports(reports.data))
   .then((converted) => {
-    const newReports = [];
-    const snapshot = getSnapshot('senate');
-    while (!_.isEqual(converted, snapshot)) {
-      newReports.push(converted.shift());
-    }
+    const newReports = getNewReports(converted, 'senate');
     return getTransactions(newReports, 'https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com');
   })
   .then((reports) => {
-    console.log(reports);
+    const valid = getValidTickers(reports);
+    email.push(prepareText(valid));
+  })
+  .then(() => getReports('https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/filemap.xml'))
+  .then((reports) => convertReports(reports.data))
+  .then((converted) => {
+    const newReports = getNewReports(converted, 'house');
+    fileOutput = converted;
+    return getTransactions(newReports, 'https://house-stock-watcher-data.s3-us-west-2.amazonaws.com');
+  })
+  .then((reports) => {
+    const valid = getValidTickers(reports);
+    email.push(prepareText(valid));
+  })
+  .then(() => {
+    console.log(email);
   });
